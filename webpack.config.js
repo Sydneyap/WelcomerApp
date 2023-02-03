8177657861846c5e04ff23e5700c4845ffd62426
Sydneyap <64597348+A-randomperson@users.noreply.github.com>
2023-02-03 13:12:09 -0500

    Got started on website

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+//core node module
+module.exports = {
+  //export object from file
+  mode: 'development',
+  entry: './src/index.js',
+  //Specify the file to look for javascript source file
+  output: {
+    //This is an object
+    path: path.resolve(__dirname, 'dist'),
+    //path to whatever folder  we want object file to be put in
+    //__dirname gets directory of file
+    //Go into dist file
+    //Needs to get absolute path not relative
+    filename: 'bundle.js'
+  },
+  watch: true
+  //When webpack run, it will watch for changes in index.js
+  //Spits out bundle into bundle.js
+}
