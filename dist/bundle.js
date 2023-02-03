3c1f8ec708e6d31ef9fbb54aa2df3e46979073d1
Sydneyap <64597348+A-randomperson@users.noreply.github.com>
2023-02-03 13:39:50 -0500

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log('hello from index.js')\n\n\n//# sourceURL=webpack://WelcomerApp/./src/index.js?");
+eval("throw new Error(\"Module parse failed: Unexpected token (1:19)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> import {initialize app} from 'firebase/app'\\n| import {\\n|   getFirestore, collection\");\n\n//# sourceURL=webpack://WelcomerApp/./src/index.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("console.log('hello from index.js')\n\n\n//# sourceURL=webpack://WelcomerAp
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello from index.js')
+import {initialize app} from 'firebase/app'
+import {
+  getFirestore, collection
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJqSskt44fzdYxgH3Orx8j6Mrr6C5IfXI",
+  authDomain: "welcomer-622.firebaseapp.com",
+  projectId: "welcomer-622",
+  storageBucket: "welcomer-622.appspot.com",
+  messagingSenderId: "243775247254",
+  appId: "1:243775247254:web:91085cb7331b43b90543b7"
+};
+
+initializeApp(firebaseConfig)
+
+const db = getFirestore()
+
+const colRef = collection(db, 'User')
+
+getDocs(colRef)
+  .then((snapshot) => {
+    console.log(snapshot.docs)
+  })
